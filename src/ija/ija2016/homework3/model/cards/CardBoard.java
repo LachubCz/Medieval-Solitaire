04b743f515bf8912693110fee8720d468be74ca4
LachubCz <petr.genius.buchal@gmail.com>
2017-05-05 21:07:48 +0200

    Update CardBoard.java

--- src/ija/ija2016/homework3/model/cards/CardBoard.java
+++ src/ija/ija2016/homework3/model/cards/CardBoard.java
@@ -2,12 +2,14 @@ package ija.ija2016.homework3.model.cards;
 
 import ija.ija2016.homework3.model.board.AbstractFactorySolitaire;
 import ija.ija2016.homework3.model.board.FactoryKlondike;
+import ija.ija2016.homework3.model.cards.CardDeck;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class CardBoard implements CardBoardInterface{
-        private AbstractFactorySolitaire factory;
-    
-        protected CardStack SourcePack = null;
+        protected CardDeck SourcePack = null;
         protected ArrayList<RepaintInterface> observers;
         protected ArrayList<CardStack> WorkingStacks;
         protected ArrayList<CardDeck> Targetdecks;
@@ -16,33 +18,79 @@ public class CardBoard implements CardBoardInterface{
         public static final String saveExtension = ".XXX";
         
         public CardBoard() {
-        this.Targetdecks = new ArrayList<>();
-        this.WorkingStacks = new ArrayList<>();
-        this.observers = new ArrayList<>();
-        this.factory = new FactoryKlondike(); 
-        
-        
-        
-        
-            
+	        this.Targetdecks = new ArrayList<>();
+	        this.WorkingStacks = new ArrayList<>();
+	        this.observers = new ArrayList<>();
+	        AbstractFactorySolitaire Maker = new FactoryKlondike(); 
+	        
+	        this.SourcePack = Maker.createCardDeck();
+	        this.SourcePack = RandomSwap(SourcePack);
+	        
+	        for (int i = 0; i < 7; i++)
+	        {
+	        	this.WorkingStacks.add(Maker.createWorkingPack());
+	            for(int u = 0; u <= i; u++)
+	            {
+	            	if (u == i)
+	            	{
+	            		Card c = this.SourcePack.pop();
+	            		c.turnFaceUp();
+	            		this.WorkingStacks.get(i).InitPut(c);
+	            	}
+	            	else
+	            		this.WorkingStacks.get(i).InitPut(this.SourcePack.pop());
+	            }
+	        }
+	        
+	        this.Targetdecks.add(Maker.createTargetPack(Card.Color.DIAMONDS));
+	        this.Targetdecks.add(Maker.createTargetPack(Card.Color.HEARTS));
+	        this.Targetdecks.add(Maker.createTargetPack(Card.Color.SPADES));
+	        this.Targetdecks.add(Maker.createTargetPack(Card.Color.CLUBS));
         }
     
-
-	public void registerObserver(RepaintInterface repaintInterface) {
-		// TODO Auto-generated method stub
+		@Override
+		public CardStack getSourcePack() {
+			// TODO Auto-generated method stub
+			return null;
+		}
 		
-	}
+        public void registerObserver(RepaintInterface repaintInterface) {
+        	;// TODO Auto-generated method stub
+        }
         
-        public CardStack getSourcePack(){
-        return this.SourcePack;
+        public CardDeck GetSourceDeck()
+        {
+            return this.SourcePack;
         }
-
-        public CardDeck getDeck(int index) {
-            return this.Targetdecks.get(index);
+        
+        public CardDeck getDeck(int index)
+        {
+        	return this.Targetdecks.get(index);
         }
-
-        public CardStack getStack(int index) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        public CardStack getStack(int index)
+        {
+        	return this.WorkingStacks.get(index);
         }
-
+        
+        public boolean LoadGame ()
+        {
+            return true;
+        }
+        
+        public boolean SaveGame ()
+        {
+            return true;
+        }
+        
+        public CardDeck RandomSwap(CardDeck Deck)
+        {
+            Random NumberGenerator = new Random();
+            for (int i = 0; i < 52; i++)
+            {
+                int RandomCard = NumberGenerator.nextInt(51);
+                Collections.swap(Deck.karty, i, RandomCard);
+            }
+            return Deck;
+        }
 }
