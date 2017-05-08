0efa45f38a9da883d614a22b571dadb8c0f05dc0
LachubCz <petr.genius.buchal@gmail.com>
2017-05-08 16:08:09 +0200

    Update CommandNext.java

--- src/ija/ija2016/homework3/controller/CommandNext.java
+++ src/ija/ija2016/homework3/controller/CommandNext.java
@@ -1,35 +1,53 @@
+/*
+ * @author Petr Buchal, xbucha02
+ * @author Tomas Holik, xholik13
+ * @version 1.0
+ * Project: Medieval Klondike
+ * University: Brno University of Technology
+ * Course: IJA
+ */
 package ija.ija2016.homework3.controller;
-
 
 import ija.ija2016.homework3.model.cards.CardStack;
 
-/**
- *
- * @author Holajz
- */
+public class CommandNext implements CommandInterface
+{
+	CardStack stack = null;
 
-public class CommandNext implements CommandInterface{
-    CardStack stack = null;
+	/**
+	 * [CommandNext description]
+	 * @param  stack [cil]
+	 */
+	public CommandNext(CardStack stack)
+	{
+		this.stack = stack;
+	}
 
-    public CommandNext(CardStack stack){
-        this.stack = stack;
-    }
+	/**
+	 * [metoda vykona prikaz]
+	 */
+	public void execute()
+	{
+		if(this.canExecute()) 
+		{
+			stack.showNext();
+		}
+	}
 
-    @Override
-    public void execute(){
-        if(this.canExecute()) {
-            stack.showNext();
-        }
+	/**
+	 * [metoda pro vraceni do stavu pred provedenim prikazu]
+	 */
+	public void unexecute()
+	{
+		stack.hideTopCard();
+	}
 
-    }
-
-    @Override
-    public void unexecute(){
-        stack.hideTopCard();
-    }
-
-    @Override
-    public boolean canExecute(){
-        return !this.stack.isEmpty();
-    }
+	/**
+	 * [metoda zjisti zda-li jde prikaz vykonat]
+	 * @return [vraci true pokud lze prikaz provest]
+	 */
+	public boolean canExecute()
+	{
+		return !this.stack.isEmpty();
+	}
 }
