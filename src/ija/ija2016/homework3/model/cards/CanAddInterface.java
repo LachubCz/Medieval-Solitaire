aa414090d468ee3d209df6d63062e1f8064d3d2d
LachubCz <petr.genius.buchal@gmail.com>
2017-05-08 16:10:38 +0200

    Update CanAddInterface.java

--- src/ija/ija2016/homework3/model/cards/CanAddInterface.java
+++ src/ija/ija2016/homework3/model/cards/CanAddInterface.java
@@ -1,18 +1,19 @@
 /*
- * @author Tom� Hol�k, xholik13
  * @author Petr Buchal, xbucha02
- * @version 0.3
- * Projekt: Medieval Klondike
- * System: Windows 10
+ * @author Tomas Holik, xholik13
+ * @version 1.0
+ * Project: Medieval Klondike
  * University: Brno University of Technology
  * Course: IJA
  */
 package ija.ija2016.homework3.model.cards;
 
-/**
- *
- * @author Holajz
- */
-public interface CanAddInterface {
-    public void add(CardDeck deck, int count);
+public interface CanAddInterface 
+{
+	/**
+	 * [add description]
+	 * @param deck  [description]
+	 * @param count [description]
+	 */
+	public void add(CardDeck deck, int count);
 }
