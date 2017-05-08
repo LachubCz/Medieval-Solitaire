1114f02ca96f7b73e2b2b6cc40c379593fe5ae56
LachubCz <petr.genius.buchal@gmail.com>
2017-05-08 16:12:20 +0200

    Update CardStackInterface.java

--- src/ija/ija2016/homework3/model/cards/CardStackInterface.java
+++ src/ija/ija2016/homework3/model/cards/CardStackInterface.java
@@ -1,34 +1,35 @@
-/**
- * Rozhran� reprezentuj�c� pracujici pole, rozsiruje balicek karet.
- * @author Tom� Hol�k, xholik13
+/*
  * @author Petr Buchal, xbucha02
- * @version 0.2
+ * @author Tomas Holik, xholik13
+ * @version 1.0
+ * Project: Medieval Klondike
+ * University: Brno University of Technology
+ * Course: IJA
  */
-
 package ija.ija2016.homework3.model.cards;
 
 import java.io.Serializable;
 
-/**
- * rozhrani reprezentujici pracujici pole, rozsiruje balicek karet
- * @author Tomas Holik, xholik13
- */
-public interface CardStackInterface extends Serializable, CanAddInterface{
+public interface CardStackInterface extends Serializable, CanAddInterface
+{	
 	/**
-	 * Vlo�� karty ze z�sobn�ku stack na vrchol z�sobn�ku. Karty vkl�d� ve stejn�m po�ad�, v jak�m jsou uvedeny v z�sobn�ku stack.
-	 * @param stack - zasobnik vkladanych karet
-	 * @return Usp�nost akce.
+	 * [vlozi na vrchol balicek karet s kontrolou podminek]
+	 * @param  stack [vkladany balicek karet]
+	 * @return	   [pokud probehlo vlozeni uspesne vraci true]
 	 */
 	public abstract boolean put(CardStack stack);
 	
 	/**
-	 * Metoda odebere ze z�sobn�ku sekvenci karet od zadan� karty a� po vrchol z�sobn�ku. Pokud je hledan� karta na vrcholu, bude v sekvenci pouze jedna karta.
-	 * @param card - Hledan� karta.
-	 * @return Z�sobn�k karet obsahuj�c� odebranou sekvenci. Pokud hledan� karta v z�sobn�ku nen�, vrac� null.
+	 * [metoda odebere ze zasobniku sekvenci karet od zadane karty az po vrchol zasobniku, pokud je hledana karta na vrcholu, bude v sekvenci pouze jedna karta]
+	 * @param  card [hledana karta]
+	 * @return	  [zasobnik karet obsahujici odebranou sekvenci, pokud hledana karta v zasobniku neni, vraci null]
 	 */
 	public abstract CardStack pop(Card card);
 
-	
-	
+	/**
+	 * [add description]
+	 * @param deck          [description]
+	 * @param cardsToInsert [description]
+	 */
 	public abstract void add(CardDeck deck, int cardsToInsert);
 }
